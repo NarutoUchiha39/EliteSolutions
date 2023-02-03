065a2d0cdef0170282f3a22f85743532ccd12cdd
NarutoUchiha39 <anirudhata123@gmail.com>
2023-02-03 21:37:26 +0530

    Added GIF

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,9 +22,10 @@
 
  
   <div data-aos="fade-up-right" style="margin-bottom:90px">
+    <img class="img" src="{{asset('/Assets/images/coding.gif')}}"/>
     <h3 class="title-text">Why use Elite Solutions?</h3>
     <h5 class="title-text">There are many websites that provide Leetcode <br>solutions, so what makes Elite Solutions stand out? <br>We will provide you with some salient features that <br>make Elite Solutions stand out!</h5>
-    </td>
+
   </div>
   <br>
 <br>
